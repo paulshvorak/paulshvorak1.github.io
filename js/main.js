bc5cacd7a139299ab9c2b26965e077a4694e78e1
paulshvorak <paulshvorak@gmail.com>
2017-05-03 19:01:46 +0300

    1.0

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+;(function(){
+
+	// Get elements
+
+	var menuBtn = document.querySelector('.menu-trigger');
+	var closeBtn = document.querySelector('.close-btn');
+	var menu = document.querySelector('.menu');
+
+	// Functions
+
+	function openMobileMenu() {
+
+		menu.classList.add('menu--active');
+		
+		setTimeout(function() {
+			closeBtn.classList.add('close-btn--active');
+		}, 300);
+
+	}
+
+	function closeMobileMenu() {
+		menu.classList.remove('menu--active');
+		closeBtn.classList.remove('close-btn--active');
+	}
+
+	// EventListeners
+
+	menuBtn.addEventListener('click', openMobileMenu, false);
+	closeBtn.addEventListener('click', closeMobileMenu, false);
+
+}());
